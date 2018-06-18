b791c0327c0e4f85b2589159f3e4719e22cfd1b3
mozukraken <mozukraken@gmail.com>
2018-06-19 06:21:26 +0900

    次のカードの表示

--- js/main.js
+++ js/main.js
@@ -10,12 +10,28 @@
   ];
 
   var card = document.getElementById('card');
+  var cardFront = document.getElementById('card-front');
+  var cardBack = document.getElementById('card-back');
+  var btn = document.getElementById('btn');
+
   card.addEventListener('click', function() {
     flip();
+  });
+
+  btn.addEventListener('click', function() {
+    next();
   });
 
   function flip() {
     card.className = card.className === '' ? 'open' : '';
   }
+
+  function next() {
+    var num = Math.floor(Math.random() * words.length);
+    cardFront.innerHTML = words[num]['en'];
+    cardBack.innerHTML = words[num]['ja'];
+  }
+
+  next();
 
 })();
